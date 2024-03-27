03eed1f2c129de851afe1a401ca2e97e191de022
Alexander <protonull@protonmail.com>
2024-03-27 17:22:39 +0000

    Add ModMenu support

---
+++ src/main/java/uk/protonull/civvoxelmap/integrations/modmenu/ModMenuIntegration.java
@@ -0,0 +1,19 @@
+package uk.protonull.civvoxelmap.integrations.modmenu;
+
+import com.mamiyaotaru.voxelmap.gui.GuiMinimapOptions;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import java.util.Map;
+import org.jetbrains.annotations.NotNull;
+
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public @NotNull ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return GuiMinimapOptions::new;
+    }
+
+    @Override
+    public Map<String, ConfigScreenFactory<?>> getProvidedConfigScreenFactories() {
+        return Map.of("voxelmap", GuiMinimapOptions::new);
+    }
+}
