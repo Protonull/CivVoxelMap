b8802b3915cdb723f3185e68709ff1a96ecca8e3
Alexander <protonull@protonmail.com>
2024-06-23 20:38:17 +0100

    Wrap VoxelMap's checkForWaypoints in a try-catch
    This has been throwing exceptions in a rare case of CombatRadar and AdvancedChat. It's awkward because this throws before the offending chat message gets printed to the log.

---
+++ src/main/java/uk/protonull/civvoxelmap/mixins/FabricModVoxelMapMixin.java
@@ -0,0 +1,34 @@
+package uk.protonull.civvoxelmap.mixins;
+
+import com.mamiyaotaru.voxelmap.VoxelConstants;
+import com.mamiyaotaru.voxelmap.fabricmod.FabricModVoxelMap;
+import com.mamiyaotaru.voxelmap.util.CommandUtils;
+import net.minecraft.client.GuiMessageTag;
+import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(FabricModVoxelMap.class)
+public abstract class FabricModVoxelMapMixin {
+    @Redirect(
+        method = "onChat",
+        at = @At(
+            value = "INVOKE",
+            target = "Lcom/mamiyaotaru/voxelmap/util/CommandUtils;checkForWaypoints(Lnet/minecraft/network/chat/Component;Lnet/minecraft/client/GuiMessageTag;)Z"
+        )
+    )
+    protected boolean cvm$ensureSafeChatHandling(
+        final @NotNull Component chat,
+        final @NotNull GuiMessageTag indicator
+    ) {
+        try {
+            return CommandUtils.checkForWaypoints(chat, indicator);
+        }
+        catch (final Exception thrown) {
+            VoxelConstants.getLogger().warn("Something went wrong while checking for waypoints on message: {}", chat.getString(), thrown);
+            return true; // Keep the chat message
+        }
+    }
+}
