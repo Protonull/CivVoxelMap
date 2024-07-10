f1319f7cf33282788ca40639c9656937a93edd0e
Alexander <protonull@protonmail.com>
2024-07-10 03:53:05 +0100

    Convert to Gradle kts

---
+++ build.gradle.kts
@@ -0,0 +1,104 @@
+// Update Gradle Wrapper using: ./gradlew wrapper --distribution-type bin --gradle-version <version>
+// See Gradle's releases here: https://gradle.org/releases/
+
+plugins {
+    id("fabric-loom") version "1.6-SNAPSHOT"
+}
+
+version = "${rootProject.extra["mod_version"]}-${project.extra["minecraft_version"]}"
+group = "${rootProject.extra["maven_group"]}.mod.fabric"
+
+base {
+    archivesName.set(project.extra["archives_base_name"] as String)
+}
+
+loom {
+    accessWidenerPath = file("src/main/resources/civvoxelmap.accesswidener")
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${project.extra["minecraft_version"]}")
+    loom {
+        @Suppress("UnstableApiUsage")
+        mappings(layered {
+            officialMojangMappings()
+            parchment("org.parchmentmc.data:parchment-${project.extra["minecraft_version"]}:${project.extra["parchment_version"]}@zip")
+        })
+    }
+
+    modImplementation("net.fabricmc:fabric-loader:${project.extra["fabric_loader_version"]}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.extra["fabric_api_version"]}")
+
+    // https://modrinth.com/mod/voxelmap-updated/version/1.20.4-1.12.17
+    "maven.modrinth:voxelmap-updated:VYowiAJp".also {
+        modImplementation(it)
+        include(it)
+    }
+
+    // https://modrinth.com/mod/modmenu/version/9.0.0
+    "maven.modrinth:modmenu:sjtVVlsA".also {
+        modCompileOnly(it)
+        modLocalRuntime(it)
+    }
+}
+
+repositories {
+    maven(url = "https://maven.parchmentmc.org") {
+        name = "ParchmentMC"
+    }
+    maven(url = "https://api.modrinth.com/maven") {
+        name = "Modrinth"
+        content {
+            includeGroup("maven.modrinth")
+        }
+    }
+}
+
+java {
+    withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks {
+    jar {
+        from("LICENCE") {
+            rename { "LICENSE_${project.extra["mod_name"]}"} // Use US spelling
+        }
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+        options.release.set(17)
+    }
+    processResources {
+        filesMatching("fabric.mod.json") {
+            expand(
+                "mod_name" to project.extra["mod_name"],
+                "mod_version" to project.version,
+                "mod_description" to project.extra["mod_description"],
+                "copyright_licence" to project.extra["copyright_licence"],
+
+                "mod_home_url" to project.extra["mod_home_url"],
+                "mod_source_url" to project.extra["mod_source_url"],
+                "mod_issues_url" to project.extra["mod_issues_url"],
+
+                "minecraft_version" to project.extra["minecraft_version"],
+                "fabric_loader_version" to project.extra["fabric_loader_version"],
+            )
+        }
+    }
+    register<Delete>("cleanJar") {
+        delete(fileTree("./dist") {
+            include("*.jar")
+        })
+    }
+    register<Copy>("copyJar") {
+        dependsOn(getByName("cleanJar"))
+        from(getByName("remapJar"))
+        into("./dist")
+        rename("(.*?)\\.jar", "\$1-fabric.jar")
+    }
+    build {
+        dependsOn(getByName("copyJar"))
+    }
+}
