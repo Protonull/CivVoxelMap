6056c71b3f6e6e5db05ef77ccfcc6591ae6f65d9
Alexander <protonull@protonmail.com>
2024-07-10 04:46:54 +0100

    Update obscure elevation mixin

--- src/main/java/uk/protonull/civvoxelmap/mixins/elevation/ObscureElevationMixin.java
+++ src/main/java/uk/protonull/civvoxelmap/mixins/elevation/ObscureElevationMixin.java
@@ -1,27 +1,38 @@
-package uk.protonull.civvoxelmap.mixins;
+package uk.protonull.civvoxelmap.mixins.elevation;
 
 import com.mamiyaotaru.voxelmap.VoxelMap;
 import com.mamiyaotaru.voxelmap.util.Contact;
 import com.mamiyaotaru.voxelmap.util.GameVariableAccessShim;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import uk.protonull.civvoxelmap.config.ExtraRadarSettings;
 
+/**
+ * Mixins aren't really sophisticated enough to target the code within the spaghetti of VoxelMap that darkens/fades
+ * "contacts" that are above or below the player, so the solution here is to set this "contacts" Y-level to be the same
+ * as the player.
+ */
 @Mixin(Contact.class)
-public abstract class ContactMixin {
+public abstract class ObscureElevationMixin {
+    @Shadow(
+        remap = false
+    )
+    public int y;
+
     @Inject(
         method = "updateLocation",
         at = @At("TAIL"),
         remap = false
     )
-    public void cvm_inject$obscureElevation(
+    protected void cvm$updateLocation$resetElevation(
         final @NotNull CallbackInfo ci
     ) {
         if (((ExtraRadarSettings.Accessor) VoxelMap.radarOptions).hideElevation()) {
-            ((Contact) (Object) this).y = GameVariableAccessShim.yCoord();
+            this.y = GameVariableAccessShim.yCoord();
         }
     }
 }
