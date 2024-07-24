6343774306bc5ae8077cde8de625f794714b2801
Alexander <protonull@protonmail.com>
2024-07-24 04:38:05 +0100

    Fix NPE from CompressibleMapData.createKeyFromCurrentBlocks

---
+++ src/main/java/uk/protonull/civvoxelmap/mixins/catching/FixNullBlockStateIdMixin.java
@@ -0,0 +1,42 @@
+package uk.protonull.civvoxelmap.mixins.catching;
+
+import com.mamiyaotaru.voxelmap.persistent.CompressibleMapData;
+import org.jetbrains.annotations.NotNull;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+/**
+ * TODO: This should no longer be an issue after 1.21 update
+ * @see <a href="https://github.com/fantahund/VoxelMap/commit/2982c416fd8ecf4c0331bc103e0c11bf65e511cc">2982c416fd8ecf4c0331bc103e0c11bf65e511cc</a>
+ */
+@Mixin(CompressibleMapData.class)
+public abstract class FixNullBlockStateIdMixin {
+    @ModifyVariable(
+        method = "createKeyFromCurrentBlocks",
+        at = @At(
+            value = "INVOKE",
+            target = "Lcom/mamiyaotaru/voxelmap/persistent/CompressibleMapData;setData(IIIB)V",
+            shift = At.Shift.BEFORE,
+            opcode = Opcodes.ASTORE
+        ),
+        remap = false
+    )
+    protected @NotNull Integer cvm$createKeyFromCurrentBlocks$zeroOutNullIds(
+        final Integer id
+    ) {
+        return id == null ? 0 : id;
+    }
+
+    @ModifyVariable(
+        method = "getIDFromState",
+        at = @At("TAIL"),
+        remap = false
+    )
+    protected @NotNull Integer cvm$getIDFromState$zeroOutNullIds(
+        final Integer id
+    ) {
+        return id == null ? 0 : id;
+    }
+}
